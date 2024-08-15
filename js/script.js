9160d6d7e6cdd194da305dbdc3af1f71c9b767c4
vika.ritter.03@gmail.com <vika.ritter.03@gmail.com>
2024-08-15 13:34:34 +0500

    feat: add title display

--- js/script.js
+++ js/script.js
@@ -1,8 +1,11 @@
 let postTitle = '';
 const titleInputNode = document.querySelector('.js-title-input');
 const newPostBtnNode = document.querySelector('.js-new-post-btn');
+const postsNode = document.querySelector('.js-posts');
 
 newPostBtnNode.addEventListener('click', function() {
    postTitle = titleInputNode.value;
+
+   postsNode.innerText = postTitle;
 
 });
