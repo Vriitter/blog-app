f34f9fd52ddd8a8a874bb11fc5338fe824538642
vika.ritter.03@gmail.com <vika.ritter.03@gmail.com>
2024-08-15 13:15:52 +0500

    feat: add post title input

--- js/script.js
+++ js/script.js
@@ -1,1 +1,8 @@
-!
+let postTitle = '';
+const titleInputNode = document.querySelector('.js-title-input');
+const newPostBtnNode = document.querySelector('.js-new-post-btn');
+
+newPostBtnNode.addEventListener('click', function() {
+   postTitle = titleInputNode.value;
+
+});
